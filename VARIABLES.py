e5e7b36c3c86b25caedf255cb98fbcd9ec6a22a3
r244588h <ashleighnokutenda54@gmail.com>
2025-09-08 13:01:59 +0200

    Add files via upload

---
+++ VARIABLES.py
@@ -0,0 +1,16 @@
+PI = 3.1415
+
+radius = 5
+area = PI * radius * radius
+
+print("--- Circle Area Calculation ---")
+print("Value of the constant PI:", PI)
+print("Radius of the circle:", radius)
+print("The calculated area of the circle is:", area)
+
+radius = 10
+area = PI * radius * radius
+
+print("\n--- After updating the radius ---")
+print("Updated radius of the circle:", radius)
+print("The new calculated area of the  circle is - :", area)
